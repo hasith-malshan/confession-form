93e043d8cff3ac731506b3cb0c2918ad5a1e996f
Hasith Malshan <hasher22542@gmail.com>
2026-03-06 21:06:48 +0530

    Initial Commit

---
+++ src/main/java/com/hasithmalshan/confession_form/ConfessionFormApplication.java
@@ -0,0 +1,13 @@
+package com.hasithmalshan.confession_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfessionFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfessionFormApplication.class, args);
+	}
+
+}
