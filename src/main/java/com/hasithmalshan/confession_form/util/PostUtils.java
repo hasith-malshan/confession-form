bd2d6741b8a78fdfba4e0b53439af2a3b25113b3
Hasith Malshan <hasher22542@gmail.com>
2026-03-07 13:47:12 +0530

    Remove unused methods and imports from PostUtils.java

--- src/main/java/com/hasithmalshan/confession_form/util/PostUtils.java
+++ src/main/java/com/hasithmalshan/confession_form/util/PostUtils.java
@@ -1,15 +1,4 @@
 package com.hasithmalshan.confession_form.util;
 
-import com.hasithmalshan.confession_form.dto.PostReactDTO;
-import com.hasithmalshan.confession_form.dto.PostReactResponseDTO;
-import com.hasithmalshan.confession_form.repo.PostRepository;
-
 public final class PostUtils {
-
-    public static PostReactResponseDTO countReacts(PostReactDTO postReactDTO) {
-
-
-
-        return ;
-    }
 }
